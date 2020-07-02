5d2c33424ea6cab818cb339d2aebfd054af0b15f
cedalg <63453012+cedalg@users.noreply.github.com>
2020-07-02 12:08:47 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1INxDLFiqDBa5gKkbHX1J_mEVHfkSRq0U/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1INxDLFiqDBa5gKkbHX1J_mEVHfkSRq0U'
 export_file_name = 'export.pkl'
 
 classes = ['tennis', 'badminton', 'ping-pong']
